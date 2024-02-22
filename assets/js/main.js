66a3d098c8b3003b542b62ebb6dd0c170ddf3ff3
mattemutti <mattemutti@gmail.com>
2024-02-22 14:43:46 +0100

    add promp and if/else condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,29 @@
 
 
 // - creare un'array con una lista di mail
-
-
 const mailList = ['giova@gmail.com', 'lulu@gmail.com', 'martin@email.it', 'titti@libero.it', 'superwoman@mac.com'];
 
 console.log(mailList);
 
 
-
 // - chiedere all'utente la sua mail con un prompt
+const userMail = prompt('Inserisci la tua mail')
+	
+console.log(userMail);
+
+
 // - confrontare che la mail sia all'interno dell'array
+
+for (let i = 0; i < mailList.length; i++) {
+	const element = mailList[i];
+	console.log(element);
+	
+	if (userMail === element){
+		console.log('Mail presente');
+	} else {
+		console.log('mail non presente');
+	}
+}
+
+
 // - stampare in console il risultato

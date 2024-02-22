71be790cf5ae176beca6360d6786633796a89c0d
mattemutti <mattemutti@gmail.com>
2024-02-22 14:37:12 +0100

    add initial site and array

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// - Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+// - stampa un messaggio appropriato sull’esito del controllo.
+
+
+// - creare un'array con una lista di mail
+
+
+const mailList = ['giova@gmail.com', 'lulu@gmail.com', 'martin@email.it', 'titti@libero.it', 'superwoman@mac.com'];
+
+console.log(mailList);
+
+
+
+// - chiedere all'utente la sua mail con un prompt
+// - confrontare che la mail sia all'interno dell'array
+// - stampare in console il risultato
